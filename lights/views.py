762099100570e7ef431d98e16932ba1c023215d4
sambolias <serie@alaska.edu>
2018-10-21 23:36:30 -0800

    testing basic pin control

--- lights/views.py
+++ lights/views.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 def index(request):
     context = {}
     if request.method == 'POST':
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(4, GPIO.OUT)
         GPIO.output(4, not GPIO.input(4))
         GPIO.cleanup()
