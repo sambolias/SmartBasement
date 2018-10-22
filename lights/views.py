127801e5ed072620e5cd1271fb4f68f43c6138ef
sambolias <serie@alaska.edu>
2018-10-21 19:23:51 -0800

    added view template

--- lights/views.py
+++ lights/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("We got a basic ass page!")
+    context = {}
+    return render(request, 'lights/index.html', context)
