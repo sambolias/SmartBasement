5a0c2b52a10698b462948625e96f699571f1d8ec
sambolias <serie@alaska.edu>
2018-10-21 21:23:54 -0800

    testing basic pin control

--- lights/views.py
+++ lights/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import RPi.GPIO as GPIO
 
 def index(request):
     context = {}
+    if request.method = 'POST':
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(4, GPIO.OUT)
+        GPIO.output(4, not GPIO.input(4))
+        GPIO.cleanup()
     return render(request, 'lights/index.html', context)
