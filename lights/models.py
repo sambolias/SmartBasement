fdbd8768ddb7705d15326e85103604955e936e49
Sam Erie <serie@alaska.edu>
2018-10-27 10:14:33 -0800

    added device model to store light status and improved view

--- lights/models.py
+++ lights/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # This is a basic device, used for light
 class Device(models.Model):
-    name = models.CharField()
-    pin = models.IntegerField()
-    power = models.BooleanField()
+    name = models.CharField(max_length=50)
+    pin = models.IntegerField(default=4)
+    power = models.BooleanField(default=False)
     # TODO make status field and abstract for more device types
