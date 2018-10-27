59bf305e7007a4970da83371d943f5d1d38d8bd7
Sam Erie <serie@alaska.edu>
2018-10-27 10:07:24 -0800

    added device model to store light status and improved view

--- lights/models.py
+++ lights/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+# This is a basic device, used for light
+class Device(models.Model):
+    name = models.CharField()
+    pin = models.IntegerField()
+    power = models.BooleanField()
+    # TODO make status field and abstract for more device types
--- lights/views.py
+++ lights/views.py
@@ -1,17 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import RPi.GPIO as GPIO
+from .models import Device
 
 
 def index(request):
-    context = {}
-    if request.method == 'POST':
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(4, GPIO.OUT)
-        GPIO.output(4, not GPIO.input(4))
-        #only close resource when it goes off
-        #otherwise it won't stay on
+    light = Device.objects.filter(name='office_lights').first()
+    status = False
+    error = False
+    if light is not None:
+        status = True
+        error = True
+    context = {'light': status, 'error': error}
+    if request.method == 'POST' and light is not None:
+        try:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(4, GPIO.OUT)
+            GPIO.output(4, not GPIO.input(4))
+        except:
+            error = True
+            pass
+        # only close resource when it goes off
+        # otherwise it won't stay on
         if GPIO.input(4) == GPIO.LOW:
+            status = False
             GPIO.cleanup()
+        else:
+            status = True
+        # update context
+        context['light'] = status
+        # update device model
+        light.power = status
+        light.save()
     return render(request, 'lights/index.html', context)
-
