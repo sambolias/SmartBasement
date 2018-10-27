475c7d8f93677e87ca5c2a9b7f3ae24502f8a912
Sam Erie <serie@alaska.edu>
2018-10-27 12:24:38 -0800

    added event listener for input hopefully on right pin - fingers crossed

--- lights/models.py
+++ lights/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import RPi.GPIO as GPIO
+from time import sleep
 
 
 # This is a basic device, used for light
@@ -8,3 +10,26 @@ class Device(models.Model):
     power = models.BooleanField(default=False)
     toggle = models.BooleanField(default=False)
     # TODO make status field and abstract for more device types
+
+
+# seems like as good a place as any to connect listener
+gpio = Device.objects.filter(name="office_lightswitch")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def toggle_on(channel):
+    gpio.toggle = True
+    gpio.save()
+
+
+def toggle_off(channel):
+    # wait to see if it stays off (or was slight voltage drop)
+    sleep(.5)
+    if GPIO.input(gpio.pin) == GPIO.LOW:
+        gpio.toggle = True
+        gpio.save()
+
+
+GPIO.add_event_detect(gpio.pin, GPIO.RISING, callback=toggle_on, bouncetime=1)
+GPIO.add_event_detect(gpio.pin, GPIO.FALLING, callback=toggle_off, bouncetime=1)
