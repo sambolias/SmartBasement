744650d4ef4766c9e6401b1c4ff2fc5b38759236
Sam Erie <serie@alaska.edu>
2018-10-27 13:57:01 -0800

    added event listener for input hopefully on right pin - fingers crossed

--- lights/views.py
+++ lights/views.py
@@ -9,7 +9,7 @@ def index(request):
     gpio = Device.objects.filter(name="office_lightswitch").first()
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(gpio.pin, GPIO.IN)
-    msg = str(GPIO.input(gpio.pin))+' Volts for pin '+str(gpio.pin)
+    msg =  str(GPIO.input(gpio.pin))+' Volts for pin '+str(gpio.pin)
 
     light = Device.objects.filter(name='office_lights').first()
     error = False
