bb7a94e78a9e704ca4bfc191cb073ac8789fce08
Sam Erie <serie@alaska.edu>
2018-10-27 20:05:43 -0800

    retrying the listener on raspbian stretch

--- lights/views.py
+++ lights/views.py
@@ -5,26 +5,26 @@ from .models import Device
 
 
 def index(request):
-    import RPi.GPIO as GPIO
-    from time import sleep
-    gpio = Device.objects.filter(name="office_lightswitch").first()
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio.pin, GPIO.IN)
-    msg = str(GPIO.input(gpio.pin))+' Volts for pin '+str(gpio.pin)
-
-    def toggle_on(channel):
-        gpio.toggle = True
-        gpio.save()
-
-    def toggle_off(channel):
-        # wait to see if it stays off (or was slight voltage drop)
-        sleep(.5)
-        if GPIO.input(gpio.pin) == GPIO.LOW:
-            gpio.toggle = True
-            gpio.save()
-
-    GPIO.add_event_detect(gpio.pin, GPIO.RISING, callback=toggle_on)
-    # GPIO.add_event_detect(gpio.pin, GPIO.FALLING, callback=toggle_off)
+    # import RPi.GPIO as GPIO
+    # from time import sleep
+    # gpio = Device.objects.filter(name="office_lightswitch").first()
+    # GPIO.setmode(GPIO.BCM)
+    # GPIO.setup(gpio.pin, GPIO.IN)
+    # msg = str(GPIO.input(gpio.pin))+' Volts for pin '+str(gpio.pin)
+    #
+    # def toggle_on(channel):
+    #     gpio.toggle = True
+    #     gpio.save()
+    #
+    # def toggle_off(channel):
+    #     # wait to see if it stays off (or was slight voltage drop)
+    #     sleep(.5)
+    #     if GPIO.input(gpio.pin) == GPIO.LOW:
+    #         gpio.toggle = True
+    #         gpio.save()
+    #
+    # GPIO.add_event_detect(gpio.pin, GPIO.RISING, callback=toggle_on)
+    # # GPIO.add_event_detect(gpio.pin, GPIO.FALLING, callback=toggle_off)
 
     light = Device.objects.filter(name='office_lights').first()
     error = False
