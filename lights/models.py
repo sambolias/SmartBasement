5ee94f054b209cf9e1bc8667acfe645dcef3078c
Sam Erie <=>
2018-10-27 22:06:49 -0800

    clean repo, correctly ignoring generated files, trying to rebase on existing rmote master...

---
+++ lights/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+# This is a basic device, used for light
+class Device(models.Model):
+    name = models.CharField(max_length=50)
+    pin = models.IntegerField(default=4)
+    power = models.BooleanField(default=False)
+    toggle = models.BooleanField(default=False)
+    # TODO make status field and abstract for more device types
+
+
+# seems like as good a place as any to connect listener
+#gpio = Device.objects.filter(name="office_lightswitch").first()
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(gpio.pin, GPIO.IN)
+
+
+#def toggle_on(channel):
+#    gpio.toggle = True
+#    gpio.save()
+
+
+#def toggle_off(channel):
+    # wait to see if it stays off (or was slight voltage drop)
+#    sleep(.5)
+#    if GPIO.input(gpio.pin) == GPIO.LOW:
+#        gpio.toggle = True
+#        gpio.save()
+
+
+#GPIO.add_event_detect(gpio.pin, GPIO.RISING, callback=toggle_on)
+# GPIO.add_event_detect(gpio.pin, GPIO.FALLING, callback=toggle_off)
---
+++ lights/views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from libs.lightswitch import DevSwitch
+from .models import Device
+
+
+def index(request):
+    # import RPi.GPIO as GPIO
+    # from time import sleep
+    # gpio = Device.objects.filter(name="office_lightswitch").first()
+    # GPIO.setmode(GPIO.BCM)
+    # GPIO.setup(gpio.pin, GPIO.IN)
+    # msg = str(GPIO.input(gpio.pin))+' Volts for pin '+str(gpio.pin)
+    #
+    # def toggle_on(channel):
+    #     gpio.toggle = True
+    #     gpio.save()
+    #
+    # def toggle_off(channel):
+    #     # wait to see if it stays off (or was slight voltage drop)
+    #     sleep(.5)
+    #     if GPIO.input(gpio.pin) == GPIO.LOW:
+    #         gpio.toggle = True
+    #         gpio.save()
+    #
+    # GPIO.add_event_detect(gpio.pin, GPIO.RISING, callback=toggle_on)
+    # # GPIO.add_event_detect(gpio.pin, GPIO.FALLING, callback=toggle_off)
+
+    msg = 'Office Lights'
+    light = Device.objects.filter(name='office_lights').first()
+    error = False
+    if light is None:
+        status = False
+        error = True
+    else:
+        if light.toggle:
+            error = DevSwitch.toggle(light)
+            light.toggle = False
+            light.save()
+        status = light.power
+
+    context = {'light': status, 'msg': msg, 'error': error}
+
+    # TODO determine if this should be impossible if manual switch toggle
+    # seems little chance of this happening unless someone is doing it intentionally
+    if request.method == 'POST' and not error:
+        error = DevSwitch.toggle(light)
+        # update context
+        context['light'] = light.power
+        context['error'] = error
+
+    return render(request, 'lights/index.html', context)
