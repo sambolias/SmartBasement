8e15d5baba627f600459c695b4d97fd88b62f7c1
sambolias <serie@alaska.edu>
2018-11-10 13:40:51 -0900

    made redirect on post for refresh safety

--- lights/views.py
+++ lights/views.py
@@ -1,19 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from libs.lightswitch import DevSwitch
 from .models import Device
 
 
 def index(request):
-    import RPi.GPIO as GPIO
-#    msg = 'Office Lights'
     error = False
 
     switch = Device.objects.filter(name="office_lightswitch").first()
     if switch is None:
         error = True
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(switch.pin, GPIO.OUT)
+
     msg = 'Manual switch is turned '
     if not switch.power:
         msg += 'off'
@@ -23,18 +20,6 @@ def index(request):
     site = Device.objects.filter(name='office_lights').first()
     if site is None or switch is None:
         error = True
-    # else:
-        # check if the manual switch is on (power is off but power pin is hot)
-    #    GPIO.setup(site.pin, GPIO.OUT)
-    #    if GPIO.input(site.pin) == GPIO.HIGH and not site.power:
-    #        switch.power = True
-    #        switch.save()
-    #    else:
-    #        if GPIO.input(switch.pin) == GPIO.LOW and not site.power and not switch.power:
-    #            error = DevSwitch.toggle(switch)
-    #            switch.power = False  # reset to false because toggle turned on
-    #            switch.save()
-
         # TODO comment out, this is confusing
         # TODO figure out how to handle case where switch is turned on when site has lights turned on
         # maybe one of the variable pins or some pulse that could be recognized?
@@ -46,17 +31,13 @@ def index(request):
     if request.method == 'POST' and not error:
         site.toggle = True
         site.save()
-        # if switch.power:
-        #    error = DevSwitch.toggle(switch)
-        #    site.power = switch.power
-        #else:
-        #    error = DevSwitch.toggle(site)
-        #    switch.power = site.power
         # update context
         # because toggle will change power we change it here
         # TODO maybe find a way to update more accurately
         # like js in view to update based on db listeners
-        context['light'] = not site.power
-        context['error'] = error
+        # context['light'] = not site.power
+        # context['error'] = error
+        # TODO use message for error
+        return redirect('/')
 
     return render(request, 'lights/index.html', context)
