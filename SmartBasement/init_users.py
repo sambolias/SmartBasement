5bfbb5265e647512f72ec21f259fab86c4fde7d1
sambolias <serie@alaska.edu>
2019-02-03 23:08:49 -0900

    stupid mistake

--- SmartBasement/init_users.py
+++ SmartBasement/init_users.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
-if User.objects.get(username='serie') is None:
+if User.objects.filter(username='serie') is None:
     User.objects.create_user(username='serie', password='fukitol1')
-if User.objects.get(username='kim') is None:
+if User.objects.filter(username='kim') is None:
     User.objects.create_user(username='kim', password='newmommy')
